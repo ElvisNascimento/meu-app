8ec814e128d582353a6abedfcbfcff725683c7df
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-10-07 09:05:46 -0300

    funcao de pegar o valor do input

--- src/App.js
+++ src/App.js
@@ -10,23 +10,35 @@ function Button(props)
       </a>
   );
 }
+
+
+let submit = (event)=>{
+  event.preventDefault();
+  console.log(event.target[0].value);
+  document.getElementById('valor').textContent = event.target[0].value;
+}
+
+
+let titulo = 'login';
 export default function App() {
+ 
   return (
     <div>
-      <h1 className="text-center">Login</h1>
+      <h1 className="text-center">{titulo}</h1>
       <hr/>
-      <form className="d-flex justify-content-center">
-        <input className="form text-center" type="text" name="" value="" placeholder="Email"/>
-        <input className="form text-center" type="password" name="" value="" placeholder="senha"/>
-        <button className="form text-center" type=""> Entrar </button>
+      <form onSubmit={submit} className="d-flex justify-content-center">
+        <input className="form text-center" type="text" placeholder="Email"/>
+        <input className="form text-center" type="password" placeholder="senha"/>
+        <button className="form text-center" type=""> submit </button>
 
-        <Button destino="http://google.com" className="form text-center" type="">google</Button>{/* botao com funçao de ir para o link do google */}
+        <Button destino="http://google.com" className="form text-center" type="">google</Button>
+        {/* botao com funçao de ir para o link do google */}
         <Button className="form text-center" type=""> Teste </Button>
 
         <button className="form text-center" type=""> Teste </button>  
-
       </form>
         <Tabela></Tabela>     
+        <h1 id="valor"></h1>
     </div>
   );
 }
